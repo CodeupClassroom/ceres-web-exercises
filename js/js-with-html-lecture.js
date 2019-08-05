c1b8e34cc22fb984514e640cdaa9ad5e5782142a
Justin Reich <justinreich.dev@gmail.com>
2019-08-05 15:09:50 -0500

    add lecture notes

---
+++ js/js-with-html-lecture.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// confirm returns a boolean value
+
+// prompt return the value entered by the user
+
+var num1 = prompt("Enter a number:");
+var num2 = prompt("Enter a second number:");
+
+
+
+alert("num1 multipled by num2 is: " + (num1 - num2));
