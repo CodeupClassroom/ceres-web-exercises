6f006234350f2f8715884a81e8f0ba7bf8e1ba49
Justin Reich <justinreich.dev@gmail.com>
2019-08-07 11:58:25 -0500

    add scope and IIFE examples

--- js/js-with-html-lecture.js
+++ js/js-with-html-lecture.js
@@ -1,3 +1,9 @@
+
+
+(function() {
+
+
+
 "use strict";
 
 // confirm returns a boolean value
@@ -8,5 +14,11 @@ var num1 = prompt("Enter a number:");
 var num2 = prompt("Enter a second number:");
 
 
+function helloWorld() {
+    return "Hello World";
+}
 
 alert("num1 multiplied by num2 is: " + (num1 * num2));
+
+
+})();
