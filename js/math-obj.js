37fe34ab48ada4a00865055df9ef35bfe48f1d9c
David Stephens <the.david.a.stephens@gmail.com>
2019-08-15 09:36:54 -0500

    completed math object lecture

--- js/math-obj.js
+++ js/math-obj.js
@@ -19,10 +19,11 @@
 // True, and we've been doing that by using this:
 
 var max = 100;
-var min = 20;
+var min = 0;
 var randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
 
-// console.log(randomInt);
+console.log(Math.floor(Math.random()));
+console.log(randomInt);
 
 // Math.floor() is used to round a number down to it's nearest whole number (integer). By
 // multiplying the random number from Math.random() by the maximum minus the minimum. When we add
@@ -35,20 +36,20 @@ var randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
 
 // Yeah, we can round.
 
-// console.log(Math.round(8.9));
-// console.log(Math.round(8.5));
-// console.log(Math.round(8.1));
-// console.log(Math.round(8.00000009));
+console.log(Math.round(8.9));
+console.log(Math.round(8.5));
+console.log(Math.round(8.1));
+console.log(Math.round(8.00000009));
 
 // Math.round() will round to the nearest integer. This can mean we'll round up or down depending
 // on the decimal value.
 
 // We have a floor, so wouldn't it make sense that we have a ceiling?
 
-// console.log(Math.ceil(8.9));
-// console.log(Math.ceil(8.5));
-// console.log(Math.ceil(8.1));
-// console.log(Math.ceil(8.00000009));
+console.log(Math.ceil(8.9));
+console.log(Math.ceil(8.5));
+console.log(Math.ceil(8.1));
+console.log(Math.ceil(8.0000000));
 
 // Math.ceil() will round up.
 
@@ -58,16 +59,16 @@ var randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
 
 // We can also use Math.pow() for exponents
 
-// console.log(Math.pow(4,2));
-// console.log(Math.pow(4,3));
-// console.log(Math.pow(4,4));
-// console.log(Math.pow(4,.5));
+console.log(Math.pow(4,2));
+console.log(Math.pow(4,3));
+console.log(Math.pow(4,4));
+console.log(Math.pow(4,.5));
 
 // We can also use the Math.sqrt() function to find the square root of a number
 
-// console.log(Math.sqrt(4));
-// console.log(Math.sqrt(16));
-// console.log(Math.round(Math.sqrt(Math.pow(20, 7))));
+console.log(Math.sqrt(4));
+console.log(Math.sqrt(16));
+console.log(Math.round(Math.sqrt(Math.pow(20, 7))));
 
 /**
  * "That's a lot of methods, but what about properties"
@@ -79,6 +80,6 @@ function volumeSphereRounded(radius) {
     return Math.round((4/3) * Math.PI * Math.pow(radius,3));
 }
 
-// console.log(volumeSphereRounded(12));
-
+console.log(volumeSphereRounded(12));
+//  console.log(Math);
 // We can use the pi constant by using Math.PI (note that PI is in all caps).
