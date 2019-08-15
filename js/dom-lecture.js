240a04d924aa05b8fb5c0a06328a3c55c3a74abe
Justin Reich <justinreich.dev@gmail.com>
2019-08-15 11:03:06 -0500

    add dom lecture notes

---
+++ js/dom-lecture.js
@@ -0,0 +1,76 @@
+"use strict";
+
+
+// ========== Retrieve elements by id, class, tag name
+
+// id
+
+// class - will return an HTMLCollection
+
+// tag
+
+
+// ========== Query Selector and Query Selector All
+
+// querySelector()
+
+
+
+// querySelectorAll()
+
+
+// ========== Direct access to form inputs
+
+
+// ========== Accessing and modifying elements and properties
+
+// get value of innerHTML
+
+// set value of innerHTML
+
+
+// set value of innerText
+
+
+// append value to innerText (works the same with innerHTML)
+
+
+
+// ========== Accessing and modifying attributes
+
+// check if attribute exists
+
+
+// get an attribute value
+
+
+// create a new attribute or change a value of an existing attribute
+    //     cowboy.setAttribute('data-1', 'hello');
+    //     cowboy.setAttribute('data-test', 'testing');
+    //     console.log(cowboy.getAttribute('data-1'));
+
+// remove attribute
+    //     cowboy.removeAttribute("data-test");
+    //     console.log(cowboy);
+
+
+// ========== Accessing and modifying styles
+
+// single style
+
+
+// apply multiple style changes
+    //     Object.assign(jumbotron.style, {
+    //         border: "10px solid black","font-family": "times", "text-decoration": "underline"
+    //     });
+
+// styling node list
+    //     var tableRows = document.getElementsByTagName("tr");
+    //     for (var i = 0; i < tableRows.length; i += 1) {
+    //         tableRows[i].style.background = "red";
+    //     }
+
+
+// ========== Add / Remove Elements
+
+// see curriculum links
